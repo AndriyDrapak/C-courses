a5c70974e2d0a497ad4691ca18a157e8af1cc7a4
AndriyDrapak <60966701+AndriyDrapak@users.noreply.github.com>
2020-02-19 20:13:46 +0200

    2lesson

---
+++ C1/C1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine()); 
+            int b = Int32.Parse(Console.ReadLine());
+            int c = (a + b) * 2;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
