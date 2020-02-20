3e8a21c37f12b940411f325f362635be94a738da
AndriyDrapak <60966701+AndriyDrapak@users.noreply.github.com>
2020-02-20 21:32:20 +0200

    homework

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+            int d = Int32.Parse(Console.ReadLine());
+            int e = Int32.Parse(Console.ReadLine());
+            int f = a + b + c + d + e;
+            Console.WriteLine(f);
+            Console.ReadKey();
+        }
+    }
+}
